255021f1bbb6c488dc5c9e3294e2c914fd946c13
georgebv <georgeme007@gmail.com>
2016-11-24 13:30:18 -0500

    resources

--- AppCore/test_gui.py
+++ AppCore/test_gui.py
@@ -6,7 +6,7 @@ app = QtGui.QApplication(sys.argv)
 window = QtGui.QWidget()
 window.setGeometry(50, 50, 500, 500)
 window.setWindowTitle('PyEVA')
-window.setWindowIcon(QtGui.QIcon(r'..\lambda.png'))
+window.setWindowIcon(QtGui.QIcon(r'..\Resources\lambda.png'))
 
 window.show()
 sys.exit(app.exec_())
