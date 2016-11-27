89b178c78ed2e984535f40bc5689ff0546cdc358
georgebv <georgeme007@gmail.com>
2016-11-26 23:14:40 -0500

    gui overhaul

--- AppCore/PyEVA.py
+++ AppCore/PyEVA.py
@@ -9,7 +9,7 @@ import os
 # Setup current project state (a global variable, must be declared as global in each function / method)
 # First item - GUI state, second item - internal EVA state
 #  Store all global variable in PyEVA_state
-PyEVA_state = [['save_name', 'None'], ['Series', 'None']]
+PyEVA_state = [['save_name', 'None'], ['Series', 'EVA_class']]
 
 
 class PyEVAMainWindow(QtGui.QMainWindow, Ui_MainWindow):
@@ -19,17 +19,21 @@ class PyEVAMainWindow(QtGui.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.setupUi(self)
+        self.mwUI_activator()
 
-        # Map actions
+        # Map toolbar actions
         self.actionExit.triggered.connect(self.mwExit)
         self.actionNew.triggered.connect(self.mwNew)
         self.actionHelp.triggered.connect(self.mwHelp)
         self.actionSave.triggered.connect(self.mwSave)
         self.actionOpen.triggered.connect(self.mwOpen)
+        self.actionSettings.triggered.connect(self.mwSettings)
 
         # Map buttons
         self.pushButton_2.clicked.connect(self.mwLoadSeries)
         self.pushButton.clicked.connect(self.mwParse)
+
+    # Toolbar functions
 
     def mwNew(self):
         # TODO: make <New> method work
@@ -40,9 +44,9 @@ class PyEVAMainWindow(QtGui.QMainWindow, Ui_MainWindow):
         global PyEVA_state
         print('Current save_name is: {}'.format(PyEVA_state[0][0]))
         if PyEVA_state[0][0] == 'save_name':
-            save_name = QtGui.QFileDialog.getSaveFileName(self, 'Save current PyEVA project', '/project_name')
+            save_name = QtGui.QFileDialog.getSaveFileName(self, 'Save current PyEVA project',
+                                                          '/project_name', '*.PyEVA')
             if save_name != '':
-                save_name += '.PyEVA'
                 print('Passed project save path at: {}'.format(save_name))
                 PyEVA_state[0][0] = save_name
                 with open(save_name, 'wb') as f:
@@ -58,15 +62,16 @@ class PyEVAMainWindow(QtGui.QMainWindow, Ui_MainWindow):
 
     def mwOpen(self):
         global PyEVA_state
-        load_name = QtGui.QFileDialog.getOpenFileName(self, 'Load existing PyEVA project', '/PyEVA project', '*.PyEVA')
+        load_name = QtGui.QFileDialog.getOpenFileName(self, 'Load existing PyEVA project',
+                                                      '/PyEVA project', '*.PyEVA')
         if load_name != '':
             PyEVA_state = pickle.load(open(load_name, 'rb'))
             print('Opened project at: {}'.format(PyEVA_state[0][0]))
-            # TODO: make the PyEVA_state variable update the program state
-            self.setWindowTitle(' '.join(['PyEVA -', 'project', PyEVA_state[0][0].split('/')[-1].split('.')[0]]))
-                # Button state
-        else:
-            pass
+            self.mwUI_activator()
+
+    def mwSettings(self):
+        # TODO: make <Settings> method work
+        QtGui.QMessageBox.warning(self, 'Warning', 'Settings option is not yet implemented')
 
     def mwHelp(self):
         help_dialog = QtGui.QMessageBox.about(self, 'Help', 'PyEVA, an Extreme Value Analysis application\n'
@@ -85,6 +90,8 @@ class PyEVAMainWindow(QtGui.QMainWindow, Ui_MainWindow):
         else:
             pass
 
+    # Button functions
+
     def mwParse(self):
         global PyEVA_state
         # Todo: make the <Parse> method work
@@ -101,6 +108,38 @@ class PyEVAMainWindow(QtGui.QMainWindow, Ui_MainWindow):
             # PyEVA_state[1][0] = data
         else:
             pass
+
+    def mwFit(self):
+        global PyEVA_state
+        # Do some fitting here
+        QtGui.QMessageBox.information(self, 'Fit successful', 'Distribution fit complete')
+
+    # Auxiliary functions
+
+    def mwUI_activator(self):
+        global PyEVA_state
+        if PyEVA_state[0][0] != 'save_name':
+            self.setWindowTitle(' '.join(['PyEVA -', 'project', PyEVA_state[0][0].split('/')[-1].split('.')[0]]))
+        if PyEVA_state[1][0] != 'Series':
+            self.pushButton_3.setEnabled(True)
+            self.pushButton_4.setEnabled(True)
+        else:
+            self.pushButton_3.setEnabled(False)
+            self.pushButton_4.setEnabled(False)
+        if PyEVA_state[1][1] != 'EVA_class':
+            self.comboBox_2.setEnabled(True)
+            self.checkBox.setEnabled(True)
+            self.pushButton_5.setEnabled(True)
+            self.label_4.setEnabled(True)
+            self.label_5.setEnabled(True)
+        else:
+            self.comboBox_2.setEnabled(False)
+            self.checkBox.setEnabled(False)
+            self.pushButton_5.setEnabled(False)
+            self.label_4.setEnabled(False)
+            self.label_5.setEnabled(False)
+
+
 
 def main():
     app = QtGui.QApplication(sys.argv)
